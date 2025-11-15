187077bd372c42445f7755febfcf0605bf2415ce
jijiknair <166284422+jijiknair@users.noreply.github.com>
2025-11-15 13:57:50 +0400

    Update settings.py

--- qms_project/settings.py
+++ qms_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-998ca2kr0ru=4(s*-2*1+4%1vv=acjrv_za08(qexmm1wejs)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'qmsdemo.loca.lt']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
