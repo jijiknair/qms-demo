20891e3f019a49a399b016b29c1e12badf943579
jijiknair <166284422+jijiknair@users.noreply.github.com>
2025-11-16 08:49:19 +0400

    Update settings.py

--- qms_project/settings.py
+++ qms_project/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'qms_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'qmspjt',
-        'USER': 'qms_usernew',
-        'PASSWORD': 'qmspjtoman!',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
